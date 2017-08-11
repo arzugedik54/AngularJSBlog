9016db2bac902bbdb962b739107476fbd49030e2
Murat Demirci <mdemirci@outlook.com>
2017-08-11 10:51:31 +0300

    postscreatecontroller fix

--- AngularJSBlog/ClientApp/js/controllers/PostsCreateController.js
+++ AngularJSBlog/ClientApp/js/controllers/PostsCreateController.js
@@ -1,21 +1,21 @@
 ﻿/* Setup blank page controller */
 
-angular.module('MetronicApp').controller('PostsCreateController', ['$rootScope', '$scope', '$http', 'settings', function ($rootScope, $scope, $http, settings) {
+angular.module('MetronicApp').controller('PostsCreateController', ['$rootScope', '$scope', '$http', '$location', 'settings', function ($rootScope, $scope, $http, $location, settings) {
    
-    
+    $scope.Save = function () {
+        $http({ method: "post", url: "/api/posts", data: { Name: $scope.Name, Body: $scope.Body, PublishDate: $scope.PublishDate, IsPublished: $scope.IsPublished } }).then(function (response) {
+            $location.path("/posts");
+        }, function (response) {
+            alert("Yazı kaydedilirken bir hata oluştu");
+        });
+    };
     
     $scope.$on('$viewContentLoaded', function () {   
         
         // initialize core components
         App.initAjax();
 
-        $scope.Save = function () {
-            $http.post({ url: "/api/posts", data: { Name: $scope.Name, Body: $scope.Body, PublishDate: $scope.PublishDate, IsPublished: $scope.IsPublished } }).then(function (response) {
-                alert("Yazı başarıyla kaydedildi");
-            }, function (response) {
-                alert("Yazı kaydedilirken bir hata oluştu");
-            });
-        };
+        
      
         // set default layout mode
         $rootScope.settings.layout.pageContentWhite = true;
